c832842f464ff09c3ef8cbe5a5d1863a44fa8131
lee-chaeeun <59160045+lee-chaeeun@users.noreply.github.com>
2022-08-15 14:18:33 +0200

    Add files via upload

---
+++ run.py
@@ -0,0 +1,247 @@
+import flask
+from flask import Flask,render_template,request, flash, redirect, url_for
+import plotly as py
+import plotly.express as px 
+import pandas as pd
+import numpy as np
+import csv
+import codecs
+import subprocess
+import sys
+import os
+import json
+import pickle
+import glob
+import pathlib
+import logging
+import os, datetime, operator, time
+import timeit
+
+from plotting import anomaly_scores, time_series, channelwise_scores,reconstruction_plot
+
+from configs import get_best_config
+from src.evaluation.evaluator import analyse_from_pkls
+from src.evaluation.logger_config import init_logging
+from src.evaluation.trainer import Trainer
+from configs import datasets_config, thres_methods, get_thres_config
+from src.evaluation.evaluation_utils import get_algo_class, get_dataset_class
+
+from wtforms import Form, StringField, SelectField, validators
+
+# import warnings filter
+from warnings import simplefilter
+# ignore all future warnings
+simplefilter(action='ignore', category=FutureWarning)
+
+# initialize the Flask application
+app = flask.Flask(__name__)
+app.secret_key = "super secret key"
+
+
+class smap_channels(Form):
+    choices = [('smap-A-1', 'smap-A-1'), ('smap-A-2', 'smap-A-2'), ('smap-A-3', 'smap-A-3'), ('smap-A-4', 'smap-A-4'), ('smap-A-5', 'smap-A-5'), ('smap-A-6', 'smap-A-6'), ('smap-A-7', 'smap-A-7'), ('smap-A-8', 'smap-A-8'), ('smap-A-9', 'smap-A-9'), ('smap-B-1', 'smap-B-1'), ('smap-D-1', 'smap-D-1'), ('smap-D-11', 'smap-D-11'), ('smap-D-12', 'smap-D-12'), ('smap-D-13', 'smap-D-13'), ('smap-D-2', 'smap-D-2'), ('smap-D-3', 'smap-D-3'), ('smap-D-4', 'smap-D-4'), ('smap-D-5', 'smap-D-5'), ('smap-D-6', 'smap-D-6'), ('smap-D-7', 'smap-D-7'), ('smap-D-8', 'smap-D-8'), ('smap-D-9', 'smap-D-9'), ('smap-E-1', 'smap-E-1'), ('smap-E-10', 'smap-E-10'), ('smap-E-11', 'smap-E-11'), ('smap-E-12', 'smap-E-12'), ('smap-E-13', 'smap-E-13'), ('smap-E-2', 'smap-E-2'), ('smap-E-3', 'smap-E-3'), ('smap-E-4', 'smap-E-4'), ('smap-E-5', 'smap-E-5'), ('smap-E-6', 'smap-E-6'), ('smap-E-7', 'smap-E-7'), ('smap-E-8', 'smap-E-8'), ('smap-E-9', 'smap-E-9'), ('smap-F-1', 'smap-F-1'), ('smap-F-2', 'smap-F-2'), ('smap-F-3', 'smap-F-3'), ('smap-G-1', 'smap-G-1'), ('smap-G-2', 'smap-G-2'), ('smap-G-3', 'smap-G-3'), ('smap-G-4', 'smap-G-4'), ('smap-G-6', 'smap-G-6'), ('smap-G-7', 'smap-G-7'), ('smap-P-1', 'smap-P-1'), ('smap-P-2', 'smap-P-2'), ('smap-P-3', 'smap-P-3'), ('smap-P-4', 'smap-P-4'), ('smap-P-7', 'smap-P-7'), ('smap-R-1', 'smap-R-1'), ('smap-S-1', 'smap-S-1'), ('smap-T-1', 'smap-T-1'), ('smap-T-2', 'smap-T-2'), ('smap-T-3', 'smap-T-3')]
+
+    select = SelectField('channel:', choices=choices)
+
+
+class msl_channels(Form):
+    choices = [('msl-C-1', 'msl-C-1'), ('msl-C-2', 'msl-C-2'), ('msl-D-14', 'msl-D-14'), ('msl-D-15', 'msl-D-15'), ('msl-D-16', 'msl-D-16'), ('msl-F-4', 'msl-F-4'), ('msl-F-5', 'msl-F-5'), ('msl-F-7', 'msl-F-7'), ('msl-F-8', 'msl-F-8'), ('msl-M-1', 'msl-M-1'), ('msl-M-2', 'msl-M-2'), ('msl-M-3', 'msl-M-3'), ('msl-M-4', 'msl-M-4'), ('msl-M-5', 'msl-M-5'), ('msl-M-6', 'msl-M-6'), ('msl-M-7', 'msl-M-7'), ('msl-P-10', 'msl-P-10'), ('msl-P-11', 'msl-P-11'), ('msl-P-14', 'msl-P-14'), ('msl-P-15', 'msl-P-15'), ('msl-S-2', 'msl-S-2'), ('msl-T-12', 'msl-T-12'), ('msl-T-13', 'msl-T-13'), ('msl-T-4', 'msl-T-4'), ('msl-T-5', 'msl-T-5'), ('msl-T-8', 'msl-T-8'), ('msl-T-9', 'msl-T-9')]
+
+    select = SelectField('channel:', choices=choices)    
+
+class smd_channels(Form):
+    choices = [('smd-machine-1-1', 'smd-machine-1-1'), ('smd-machine-1-2', 'smd-machine-1-2'), ('smd-machine-1-3', 'smd-machine-1-3'), ('smd-machine-1-4', 'smd-machine-1-4'), ('smd-machine-1-5', 'smd-machine-1-5'), ('smd-machine-1-6', 'smd-machine-1-6'), ('smd-machine-1-7', 'smd-machine-1-7'), ('smd-machine-1-8', 'smd-machine-1-8'), ('smd-machine-2-1', 'smd-machine-2-1'), ('smd-machine-2-2', 'smd-machine-2-2'), ('smd-machine-2-3', 'smd-machine-2-3'), ('smd-machine-2-4', 'smd-machine-2-4'), ('smd-machine-2-5', 'smd-machine-2-5'), ('smd-machine-2-6', 'smd-machine-2-6'), ('smd-machine-2-7', 'smd-machine-2-7'), ('smd-machine-2-8', 'smd-machine-2-8'), ('smd-machine-2-9', 'smd-machine-2-9'), ('smd-machine-3-1', 'smd-machine-3-1'), ('smd-machine-3-10', 'smd-machine-3-10'), ('smd-machine-3-11', 'smd-machine-3-11'), ('smd-machine-3-2', 'smd-machine-3-2'), ('smd-machine-3-3', 'smd-machine-3-3'), ('smd-machine-3-4', 'smd-machine-3-4'), ('smd-machine-3-5', 'smd-machine-3-5'), ('smd-machine-3-6', 'smd-machine-3-6'), ('smd-machine-3-7', 'smd-machine-3-7'), ('smd-machine-3-8', 'smd-machine-3-8'), ('smd-machine-3-9', 'smd-machine-3-9')]
+
+    select = SelectField('channel:', choices=choices)  
+    
+class no_channels(Form):
+    choices = [('','N/A')]
+
+    select = SelectField('channel:', choices=choices)   
+ 
+def run_multi_seeds(out_dir_root, multi_seeds, ds_to_run, algos_to_run, test_run=False):
+    for seed in multi_seeds:
+        for ds_name in ds_to_run:
+            for algo_name in algos_to_run:
+                algo_config_dict = get_best_config(algo_name=algo_name, ds_name=ds_name)
+                if test_run:
+                    if "num_epochs" in algo_config_dict.keys():
+                        algo_config_dict["num_epochs"] = 1
+                out_dir_algo = os.path.join(out_dir_root, algo_name)
+                train_analyse_algo(ds_name=ds_name, algo_name=algo_name, algo_config_dict=algo_config_dict,
+                                   out_dir_algo=out_dir_algo, seed=seed)
+
+
+def train_analyse_algo(ds_name, algo_name, algo_config_dict, out_dir_algo, seed):
+    init_logging(os.path.join(out_dir_algo, 'logs'))
+    logger = logging.getLogger(__name__)
+    if ds_name in datasets_config.keys():
+        ds_kwargs = datasets_config[ds_name]
+    else:
+        ds_kwargs = {}
+    trainer = Trainer(ds_class=get_dataset_class(ds_name),
+                      algo_seeds=[seed],
+                      algo_class=get_algo_class(algo_name),
+                      ds_seed=seed,
+                      ds_kwargs=ds_kwargs,
+                      algo_config_base=algo_config_dict,
+                      output_dir=out_dir_algo,
+                      logger=logger)
+    print(
+        "Training algo {} on dataset {} with config {} and seed {}".format(algo_name, ds_name, algo_config_dict, seed))
+    trainer.train_predict()
+    analyse_from_pkls(results_root=out_dir_algo, thres_methods=thres_methods, eval_root_cause=True, point_adjust=False,
+                      eval_dyn=True, eval_R_model=True, thres_config=get_thres_config,
+                      telem_only=True, composite_best_f1=True)
+                      
+def run_algorithm(dataset_name, algorithm_name):
+    out_dir_root = os.path.join(os.getcwd(), "reports", "trial")
+    multi_seeds = [0]
+    ds_to_run = [dataset_name]
+    algos_to_run = [
+                    "RawSignalBaseline",
+                    algorithm_name
+                    ]
+    run_multi_seeds(out_dir_root=out_dir_root,
+                    multi_seeds=multi_seeds,
+                    ds_to_run=ds_to_run,
+                    algos_to_run=algos_to_run,
+                    test_run=True)                      
+
+            
+def newest(directory):
+    files = os.listdir(directory)
+    all_subdirs = [os.path.join(directory,basename) for basename in files if os.path.isdir(os.path.join(directory,basename))]
+    newest = max(all_subdirs, key = os.path.getctime)
+    return newest
+
+def load_smap(run_path):
+    files = os.listdir(run_path)
+    all_subdirs = [os.path.join(directory,basename) for basename in files if os.path.isdir(os.path.join(directory,basename))]
+    newest = max(all_subdirs, key = os.path.getctime)
+    return newest
+    
+def load_result(data_name, algo_name):
+    out_dir_root = os.path.join(os.getcwd(), "reports", "trial") 
+    out_dir_algo = os.path.join(out_dir_root, algo_name) #reports/trial/algo_name
+    data_path = data_name + "_me" #data_me 
+    if algo_name == "VAE-LSTM":
+        search_path = os.path.join(out_dir_algo, data_path, "VAE_LSTM") #reports/trial/algo_name/data_me/algo_name
+    else:     
+        search_path = os.path.join(out_dir_algo, data_path, algo_name) #reports/trial/algo_name/data_me/algo_name
+    base_config = newest(search_path) #data_me/algo_name/base-configxxx  
+    entity_path = newest(base_config) #data_me/algo_name/base-configxxx/0-run-xxx     
+    return entity_path    
+
+# A decorator used to tell the application
+# which URL is associated function
+@app.route('/', methods =["GET", "POST"])
+def user_input():
+    if request.method == "POST":
+       # getting input with name = fname in HTML form
+       dataset_name = request.form.get("dataset_name")
+       # getting input with name = lname in HTML form
+       algorithm_name = request.form.get("algorithm_name")
+       
+       #Run multi-variate time-series anomaly detection
+       try: 
+           run_algorithm(dataset_name, algorithm_name)
+           #print("skipping for debug")  
+       except Exception as e:
+           print_on_flask = "Error running "+dataset_name+" on " + algorithm_name
+           flash(print_on_flask)
+           print(print_on_flask)
+           print(e)
+           return redirect(url_for('error', dataset_name = dataset_name, algorithm_name = algorithm_name)) 
+       else:
+           print_on_flask = "Ran "+dataset_name+" on " + algorithm_name
+           flash(print_on_flask)
+           print(print_on_flask)
+           return redirect(url_for('results', dataset_name = dataset_name, algorithm_name = algorithm_name)) 
+       
+    return render_template("form.html")
+    
+@app.route('/results/<dataset_name>/<algorithm_name>', methods=['GET', 'POST'])
+def results(dataset_name,algorithm_name):
+    dataset_name = dataset_name
+    algorithm_name = algorithm_name
+      
+    if dataset_name == "smap":
+        channel_name = dataset_name+"-A-1"  
+        #for channel-dependent plots 
+        channel_numbers = smap_channels(request.form)
+     
+    elif dataset_name == "msl":
+        channel_name = dataset_name+"-C-1"   
+        channel_numbers = msl_channels(request.form) 
+        
+    elif dataset_name == "smd":
+        channel_name = dataset_name+"-machine-1-1"
+        channel_numbers = smd_channels(request.form) 
+        
+    else:
+        channel_name = dataset_name 
+        channel_numbers = no_channels(request.form)   
+
+    if request.method == 'POST':
+        channel_name = channel_numbers.data['select']
+         
+    #load Raw Base Signal for ground truth      
+    groundtruthpath = os.path.join(load_result(dataset_name, "RawSignalBaseline"), channel_name)        
+        
+    #load raw predicitions from algorithm chosen     
+    resultpath = os.path.join(load_result(dataset_name, algorithm_name), channel_name)   
+
+    #anomaly scores ground truth 
+    with open(os.path.join(groundtruthpath, "raw_predictions"),'rb') as file:
+       ground_raw_predictions = pickle.load(file)
+       
+    #anomaly scores 
+    with open(os.path.join(resultpath, "raw_predictions"),'rb') as file:
+       raw_predictions = pickle.load(file)
+    
+    flash(ground_raw_predictions)
+    flash(raw_predictions)
+    
+    header = "Visualization of Anomaly Detection Results"
+    description = "Ran "+dataset_name+" on " + algorithm_name
+    
+    #graph og time series
+    graphJSON = time_series(dataset_name,channel_name)
+    
+    graphJSON1 = anomaly_scores(dataset_name,raw_predictions)
+    
+    graphJSON2 = channelwise_scores(ground_raw_predictions,raw_predictions)
+    
+    graphJSON3 = reconstruction_plot(ground_raw_predictions,raw_predictions)
+    
+    layout = json.dumps({'margin': dict(l=0, r=0, t=50, b=0)})
+    config = json.dumps({'displaylogo': False, 'modeBarButtonsToAdd': ['drawclosedpath', 'eraseshape']})    
+     
+    
+    return render_template("plot.html", graphJSON=graphJSON, graphJSON1=graphJSON1, graphJSON2=graphJSON2, graphJSON3=graphJSON3,layout=layout, config=config, header = header, description = description, form=channel_numbers)
+
+        
+@app.route('/error/<dataset_name>/<algorithm_name>')
+def error(dataset_name,algorithm_name):
+    dataset_name = dataset_name
+    algorithm_name = algorithm_name
+    
+    header = "Error running "+dataset_name+" on " + algorithm_name
+    description = "check terminal for error sources"
+    
+    #graph og time series
+    graphJSON = time_series(dataset_name)
+    
+    layout = json.dumps({'margin': dict(l=0, r=0, t=50, b=0)})
+    config = json.dumps({'displaylogo': False, 'modeBarButtonsToAdd': ['drawclosedpath', 'eraseshape']})
+    
+    return render_template("error.html", graphJSON=graphJSON, layout=layout, config=config, header = header, description = description)    
+        
+# first load the model and then start the server
+# we need to specify the host of 0.0.0.0 so that the app is available on both localhost as well
+# as on the external IP of the Docker container
+if __name__ == "__main__":
+
+    app.run(host='0.0.0.0', port=8080)
+
---
+++ timeseries_plot.py
@@ -0,0 +1,279 @@
+import plotly.graph_objects as go
+import plotly.io as pio
+
+import flask
+from flask import Flask,render_template,request, flash, redirect
+import plotly
+import plotly.express as px 
+
+import pandas as pd
+import numpy as np
+
+import csv
+import codecs
+import subprocess
+import sys
+import os
+import json
+import pickle
+import glob
+import pathlib
+import logging
+import datetime, operator, time
+import timeit
+import math
+import ast
+
+from src.datasets import Dataset
+from src.datasets.entities_names import smap_entities
+from src.datasets.entities_names import msl_entities
+from src.datasets.dataset import get_events
+
+#directory = os.path.join("FA","mvts-ano-eval")
+directory =os.getcwd()
+
+class Skab(Dataset):
+
+    def __init__(self, seed: int, entity=None, verbose=False):
+        """
+        :param seed: for repeatability
+        :param entity: for compatibility with multi-entity datasets. Value provided will be ignored. self.entity will be
+         set to same as dataset name for single entity datasets
+        """
+        name = "skab"
+        super().__init__(name=name, file_name="anomaly-free.csv")
+        #EDIT PATH to fit folder structure 
+        root = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
+                                           directory,"data", "raw", "skab")
+        self.raw_path_train = os.path.join(root, "anomaly-free", "anomaly-free.csv")
+        self.raw_paths_test = [os.path.join(root, folder) for folder in ["other", "valve1", "valve2"]]
+        self.seed = seed
+        self.verbose = verbose
+        self.causes = None
+
+
+    def load(self):
+        # 1 is the outlier, all other digits are normal
+        OUTLIER_CLASS = 1
+        train_df: pd.DataFrame = pd.read_csv(self.raw_path_train, index_col='datetime',sep=';',parse_dates=True)
+        print("train df len {}".format(len(train_df)))
+
+        test_df = []
+        orig_test_len = 0
+        for path_name in self.raw_paths_test:
+            path_files = os.listdir(path_name)
+            for file_name in path_files:
+                file_path = os.path.join(path_name, file_name)
+                df = pd.read_csv(file_path, index_col='datetime',sep=';', parse_dates=True)
+                # print("df of length {} clipped to {}".format(len(df), 100*(len(df)//100)))
+                orig_test_len += len(df)
+                df = df.head(100*(len(df)//100))
+                test_df.append(df)
+        test_df = pd.concat(test_df, ignore_index=True, axis=0)
+        print("orig test len {}, modified len {}".format(orig_test_len, len(test_df)	))        
+        self._data = test_df
+
+class Smap_entity(Dataset):
+
+    def __init__(self, seed: int, entity="A-1", remove_unique=False, verbose=False):
+        """
+        :param seed: for repeatability
+        """
+        if entity in smap_entities:
+            name = "smap-" + entity
+        else:
+            name = "not_found"
+            print("Entity name not recognized")
+        super().__init__(name=name, file_name="A-1.npy")
+        #EDIT PATH to fit folder structure 
+        self.base_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
+                                      directory,"data", "raw", "smap_msl")
+        self.seed = seed
+        self.remove_unique = remove_unique
+        self.entity = entity
+        self.verbose = verbose
+
+    @staticmethod
+    def one_hot_encoding(df, col_name):
+        with_dummies = pd.get_dummies(df[col_name])
+        with_dummies = with_dummies.rename(columns={name: col_name + "_" + str(name) for name in with_dummies.columns})
+        new_df = pd.concat([df, with_dummies], axis=1).drop(col_name, axis=1)
+        return new_df
+
+    def load(self):
+        # 1 is the outlier, all other digits are normal
+        OUTLIER_CLASS = 1
+        train_values = np.load(os.path.join(self.base_path, "train", self.entity + ".npy"))
+        train_df = pd.DataFrame(train_values)
+        test_values = np.load(os.path.join(self.base_path, "test", self.entity + ".npy"))
+        test_df = pd.DataFrame(test_values)
+        train_df["y"] = np.zeros(train_df.shape[0])
+
+        # Get test anomaly labels
+        test_labels = np.zeros(test_df.shape[0])
+        labels_df = pd.read_csv(os.path.join(self.base_path, "labeled_anomalies.csv"), header=0, sep=",")
+        entity_attacks = labels_df[labels_df["chan_id"] == self.entity]["anomaly_sequences"].values
+        entity_attacks = ast.literal_eval(entity_attacks[0])
+        for sequence in entity_attacks:
+            test_labels[sequence[0]:(sequence[1] + 1)] = 1
+        test_df["anomalies"] = test_labels    
+        self._data = test_df
+
+class Msl_entity(Dataset):
+
+    def __init__(self, seed: int, entity="C-1", remove_unique=False, verbose=False):
+        """
+        :param seed: for repeatability
+        """
+        if entity in msl_entities:
+            name = "msl-" + entity
+        else:
+            name = "not_found"
+            print("Entity name not recognized")
+        super().__init__(name=name, file_name="C-1.npy")
+        self.base_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
+                                      directory,"data", "raw", "smap_msl")
+        self.seed = seed
+        self.remove_unique = remove_unique
+        self.entity = entity
+        self.verbose = verbose
+
+    @staticmethod
+    def one_hot_encoding(df, col_name):
+        with_dummies = pd.get_dummies(df[col_name])
+        with_dummies = with_dummies.rename(columns={name: col_name + "_" + str(name) for name in with_dummies.columns})
+        new_df = pd.concat([df, with_dummies], axis=1).drop(col_name, axis=1)
+        return new_df
+
+    def load(self):
+        # 1 is the outlier, all other digits are normal
+        OUTLIER_CLASS = 1
+        train_values = np.load(os.path.join(self.base_path, "train", self.entity + ".npy"))
+        train_df = pd.DataFrame(train_values)
+        test_values = np.load(os.path.join(self.base_path, "test", self.entity + ".npy"))
+        test_df = pd.DataFrame(test_values)
+        train_df["y"] = np.zeros(train_df.shape[0])
+
+        # Get test anomaly labels
+        test_labels = np.zeros(test_df.shape[0])
+        labels_df = pd.read_csv(os.path.join(self.base_path, "labeled_anomalies.csv"), header=0, sep=",")
+        entity_attacks = labels_df[labels_df["chan_id"] == self.entity]["anomaly_sequences"].values
+        entity_attacks = ast.literal_eval(entity_attacks[0])
+        for sequence in entity_attacks:
+            test_labels[sequence[0]:(sequence[1] + 1)] = 1
+        test_df["anomalies"] = test_labels
+        self._data = test_df    
+        
+        
+class Smd_entity(Dataset):
+
+    def __init__(self, seed: int, entity="machine-1-1", remove_unique=False, verbose=False):
+        """
+        :param seed: for repeatability
+        """
+        name = "smd-" + entity
+        super().__init__(name=name, file_name="machine-1-1.txt")
+        self.base_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
+                                      directory,"data", "raw", "ServerMachineDataset")
+        self.seed = seed
+        self.remove_unique = remove_unique
+        self.entity = entity
+        self.verbose = verbose
+
+    @staticmethod
+    def one_hot_encoding(df, col_name):
+        with_dummies = pd.get_dummies(df[col_name])
+        with_dummies = with_dummies.rename(columns={name: col_name + "_" + str(name) for name in with_dummies.columns})
+        new_df = pd.concat([df, with_dummies], axis=1).drop(col_name, axis=1)
+        return new_df
+
+    def load(self):
+        # 1 is the outlier, all other digits are normal
+        OUTLIER_CLASS = 1
+        train_df = pd.read_csv(os.path.join(self.base_path, "train", self.entity + ".txt"), header=None, sep=",",
+                               dtype=np.float32)
+        test_df = pd.read_csv(os.path.join(self.base_path, "test", self.entity + ".txt"), header=None, sep=",",
+                              dtype=np.float32)
+        train_df["y"] = np.zeros(train_df.shape[0])
+
+        # Get test anomaly labels
+        test_labels = np.genfromtxt(os.path.join(self.base_path, "test_label", self.entity + ".txt"), dtype=np.float32,
+                                    delimiter=',')
+        test_df["anomalies"] = test_labels
+        self._data = test_df  
+        
+
+def plot_skab():
+    
+    seed = 0
+    ds = Skab(seed=seed)
+    test_df = ds.data()
+  
+    title = 'Original Time Series Plot of Skab'    
+    fig = px.line(test_df, x = test_df.index, y = list(test_df.columns), width=1000, height=400, title=title)
+    #fig_json = fig.to_json()
+    #fig.show()
+
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    print(fig.data[0])
+    
+    return graphJSON
+    
+    
+def plot_smap(channel_name):
+    
+    entity = channel_name.replace("smap-", "")
+    
+    seed = 0
+    smap = Smap_entity(seed=seed, remove_unique=False, entity = entity)
+    test_df = smap.data()
+  
+    title = 'Original Time Series Plot of SMAP '  + entity  
+    fig = px.line(test_df, x = test_df.index, y = list(test_df.columns), width=1000, height=400, title=title)
+    #fig_json = fig.to_json()
+    #fig.show()
+
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    print(fig.data[0])
+    
+    return graphJSON
+
+
+def plot_msl(channel_name):
+    entity = channel_name.replace("msl-", "")
+    
+    seed = 0
+    msl = Msl_entity(seed=seed, remove_unique=False, entity = entity)
+    test_df = msl.data()
+  
+    title = 'Original Time Series Plot of MSL-C-1'    
+    fig = px.line(test_df, x = test_df.index, y = list(test_df.columns), width=1000, height=400, title=title)
+    #fig_json = fig.to_json()
+    #fig.show()
+
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    print(fig.data[0])
+    
+    return graphJSON    
+    
+def plot_smd(channel_name):
+    entity = channel_name.replace("smd-", "")
+    
+    seed = 0
+    smd = Smd_entity(seed=seed, remove_unique=False, entity = entity)
+    test_df = smd.data()
+  
+    title = 'Original Time Series Plot of SMD-1-1'    
+    fig = px.line(test_df, x = test_df.index, y = list(test_df.columns), width=1000, height=400, title=title)
+    #fig_json = fig.to_json()
+    #fig.show()
+
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    print(fig.data[0])
+    
+    return graphJSON    
+    
