d95ee23dbd8d09002badfe937aab8b57972e9716
lee-chaeeun <59160045+lee-chaeeun@users.noreply.github.com>
2022-09-09 11:47:51 +0200

    Update timeseries_plot.py

--- timeseries_plot.py
+++ timeseries_plot.py
@@ -32,6 +32,12 @@ from src.datasets.dataset import get_events
 #directory = os.path.join("FA","mvts-ano-eval")
 directory =os.getcwd()
 
+
+"""
+@author of dataset Classes: Astha Garg 10/19 - edited to extract test data for plotting
+@author of plotting functions: Chae Eun Lee 09/07
+"""
+
 class Skab(Dataset):
 
     def __init__(self, seed: int, entity=None, verbose=False):
@@ -201,7 +207,195 @@ class Smd_entity(Dataset):
                                     delimiter=',')
         test_df["anomalies"] = test_labels
         self._data = test_df  
+
+
         
+class Swat(Dataset):
+
+    def __init__(self, seed: int, shorten_long=True, remove_unique=False, entity=None, verbose=False, one_hot=False):
+        """
+        :param seed: for repeatability
+        :param entity: for compatibility with multi-entity datasets. Value provided will be ignored. self.entity will be
+         set to same as dataset name for single entity datasets
+        """
+        if shorten_long:
+            name = "swat"
+        else:
+            name = "swat-long"
+        super().__init__(name=name, file_name="SWaT_Dataset_Normal_v1.csv")
+        root = os.path.join(os.getcwd(),"data", "raw", "swat", "raw")
+        self.raw_path_train = os.path.join(root, "SWaT_Dataset_Normal_v1.csv")
+        self.raw_path_test = os.path.join(root, "SWaT_Dataset_Attack_v0.csv")
+
+        if not os.path.isfile(self.raw_path_train):
+            df = pd.read_excel(os.path.join(root, "SWaT_Dataset_Normal_v1.xlsx"))
+            df.to_csv(self.raw_path_train, index=False)
+            
+        if not os.path.isfile(self.raw_path_test):
+            df = pd.read_excel(os.path.join(root, "SWaT_Dataset_Attack_v0.xlsx"))
+            df.to_csv(self.raw_path_test, index=False)
+
+        self.seed = seed
+        self.shorten_long = shorten_long
+        self.remove_unique = remove_unique
+        self.verbose = verbose
+        self.one_hot = one_hot
+
+    def load(self):
+        # 1 is the outlier, all other digits are normal
+        OUTLIER_CLASS = 1
+        
+        #skip first row because it is empty
+        test_df: pd.DataFrame = pd.read_csv(self.raw_path_test, skiprows = 1)
+        train_df: pd.DataFrame = pd.read_csv(self.raw_path_train, skiprows = 1)
+
+        train_df = train_df.rename(columns={col: col.strip() for col in train_df.columns})
+        test_df = test_df.rename(columns={col: col.strip() for col in test_df.columns})
+
+        train_df["y"] = train_df["Normal/Attack"].replace(to_replace=["Normal", "Attack", "A ttack"], value=[0, 1, 1])
+        train_df = train_df.drop(columns=["Normal/Attack", "Timestamp"], axis=1)
+        test_df["y"] = test_df["Normal/Attack"].replace(to_replace=["Normal", "Attack", "A ttack"], value=[0, 1, 1])
+        test_df = test_df.drop(columns=["Normal/Attack", "Timestamp"], axis=1)
+
+        # one-hot-encoding stuff
+        if self.one_hot:
+            keywords = {col_name: "".join([s for s in col_name if not s.isdigit()]) for col_name in train_df.columns}
+            cat_cols = [col for col in keywords.keys() if keywords[col] in ["P", "MV", "UV"]]
+            one_hot_cols = [col for col in cat_cols if train_df[col].nunique() >= 3 or test_df[col].nunique() >= 3]
+            print(one_hot_cols)
+            one_hot_encoded = Dataset.one_hot_encoding(pd.concat([train_df, test_df], axis=0, join="inner"),
+                                                       col_names=one_hot_cols)
+            train_df = one_hot_encoded.iloc[:len(train_df)]
+            test_df = one_hot_encoded.iloc[len(train_df):]
+
+        # shorten the extra long anomaly to 550 points
+        if self.shorten_long:
+            long_anom_start = 227828
+            long_anom_end = 263727
+            test_df = test_df.drop(test_df.loc[(long_anom_start + 551):(long_anom_end + 1)].index,
+                                   axis=0).reset_index(drop=True)
+        causes_channels_names = [["MV101"], ["P102"], ["LIT101"], [], ["AIT202"], ["LIT301"], ["DPIT301"],
+                                 ["FIT401"], [], ["MV304"], ["MV303"], ["LIT301"], ["MV303"], ["AIT504"],
+                                 ["AIT504"], ["MV101", "LIT101"], ["UV401", "AIT502", "P501"], ["P602", "DPIT301",
+                                                                                                "MV302"],
+                                 ["P203", "P205"], ["LIT401", "P401"], ["P101", "LIT301"], ["P302", "LIT401"],
+                                 ["P201", "P203", "P205"], ["LIT101", "P101", "MV201"], ["LIT401"], ["LIT301"],
+                                 ["LIT101"], ["P101"], ["P101", "P102"], ["LIT101"], ["P501", "FIT502"],
+                                 ["AIT402", "AIT502"], ["FIT401", "AIT502"], ["FIT401"], ["LIT301"]]
+                                 
+        self._data = test_df  
+
+
+class Wadi(Dataset):
+
+    def __init__(self, seed: int, remove_unique=False, entity=None, verbose=False, one_hot=False):
+        """
+        :param seed: for repeatability
+        :param entity: for compatibility with multi-entity datasets. Value provided will be ignored. self.entity will be
+         set to same as dataset name for single entity datasets
+        """
+        super().__init__(name="wadi", file_name="WADI_14days.csv")
+        self.raw_path_train = os.path.join(os.getcwd(),"data", "raw", "wadi", "raw", "WADI_14days.csv")
+        self.raw_path_test = os.path.join(os.getcwd(),"data", "raw", "wadi", "raw", "WADI_attackdata.csv")
+        self.anomalies_path = os.path.join(os.getcwd(),"data", "raw", "wadi", "raw", "WADI_anomalies.csv")
+        self.seed = seed
+        self.remove_unique = remove_unique
+        self.verbose = verbose
+        self.one_hot = one_hot
+
+    def load(self):
+        # 1 is the outlier, all other digits are normal
+        OUTLIER_CLASS = 1
+        test_df: pd.DataFrame = pd.read_csv(self.raw_path_test, header=0)
+        train_df: pd.DataFrame = pd.read_csv(self.raw_path_train, header=3)
+
+        # Removing 4 columns who only contain nans (data missing from the csv file)
+        nan_columns = [r'\\WIN-25J4RO10SBF\LOG_DATA\SUTD_WADI\LOG_DATA\2_LS_001_AL',
+                       r'\\WIN-25J4RO10SBF\LOG_DATA\SUTD_WADI\LOG_DATA\2_LS_002_AL',
+                       r'\\WIN-25J4RO10SBF\LOG_DATA\SUTD_WADI\LOG_DATA\2_P_001_STATUS',
+                       r'\\WIN-25J4RO10SBF\LOG_DATA\SUTD_WADI\LOG_DATA\2_P_002_STATUS']
+        train_df = train_df.drop(nan_columns, axis=1)
+        test_df = test_df.drop(nan_columns, axis=1)
+
+        train_df = train_df.rename(columns={col: col.split('\\')[-1] for col in train_df.columns})
+        test_df = test_df.rename(columns={col: col.split('\\')[-1] for col in test_df.columns})
+
+        # Adding anomaly labels as a column in the dataframes
+        ano_df = pd.read_csv(self.anomalies_path, header=0)
+        #train_df["y"] = np.zeros(train_df.shape[0])
+        test_df["anomalies"] = np.zeros(test_df.shape[0])
+        causes = []
+        
+        for i in range(ano_df.shape[0]):
+            ano = ano_df.iloc[i, :][["Start_time", "End_time", "Date"]]
+            start_row = np.where((test_df["Time"].values == ano["Start_time"]) &
+                                 (test_df["Date"].values == ano["Date"]))[0][0]
+            end_row = np.where((test_df["Time"].values == ano["End_time"]) &
+                               (test_df["Date"].values == ano["Date"]))[0][0]
+            test_df["anomalies"].iloc[start_row:(end_row + 1)] = np.ones(1 + end_row - start_row)
+            #causes.append(ano_df.iloc[i, :]["Causes"])
+
+        test_df = test_df.iloc[:,1:] #drop first two columns indicating row.
+        test_df["Date"] = test_df['Date'].astype(str) +" "+ test_df["Time"]
+        test_df = test_df.drop('Time', axis=1)
+        
+        self._data = test_df
+
+class Damadics(Dataset):
+
+    def __init__(self, seed: int, remove_unique=False, entity=None, verbose=False, drop_init_test=False):
+        """
+        :param seed: for repeatability
+        :param entity: for compatibility with multi-entity datasets. Value provided will be ignored. self.entity will be
+         set to same as dataset name for single entity datasets
+        """
+        if drop_init_test:
+            name = "damadics-s"
+        else:
+            name = "damadics"
+        super().__init__(name=name, file_name="31102001.txt")
+        train_filenames = ["31102001.txt"] + ["0"+str(i) + "112001.txt" for i in range(1, 9)]
+        test_filenames = ["09112001.txt", "17112001.txt", "20112001.txt"]
+        self.test_dates = [date[:-4] for date in test_filenames] # will be used to add labels to dataframes
+        self.raw_paths_train = [os.path.join(os.getcwd(), "data", "raw", "damadics", "raw", filename) for filename in train_filenames]
+        self.raw_paths_test = [os.path.join(os.getcwd(),
+                                           "data", "raw", "damadics", "raw", filename) for filename in test_filenames]
+        self.anomalies_path = os.path.join(os.getcwd(), "data", "raw", "damadics", "raw", "DAMADICS_anomalies.csv")
+        self.seed = seed
+        self.remove_unique = remove_unique
+        self.verbose = verbose
+        self.drop_init_test = drop_init_test
+
+    @staticmethod
+    def one_hot_encoding(df, col_name):
+        with_dummies = pd.get_dummies(df[col_name])
+        with_dummies = with_dummies.rename(columns={name: col_name + "_" + str(name) for name in with_dummies.columns})
+        new_df = pd.concat([df, with_dummies], axis=1).drop(col_name, axis=1)
+        return new_df
+
+    def load(self):
+        # 1 is the outlier, all other digits are normal
+        OUTLIER_CLASS = 1
+        train_dataframes = [pd.read_csv(path, header=None, sep="\t") for path in self.raw_paths_train]
+        test_dataframes = [pd.read_csv(path, header=None, sep="\t") for path in self.raw_paths_test]
+
+        # Adding anomaly labels as a column in the dataframes
+        ano_df = pd.read_csv(self.anomalies_path, header=0, dtype=str)
+        for df in test_dataframes:
+            df["y"] = np.zeros(df.shape[0])
+        for i in range(ano_df.shape[0]):
+            ano = ano_df.iloc[i, :][["Start_time", "End_time", "Date"]]
+            date = ano["Date"]
+            df_idx = self.test_dates.index(date)
+            start_row = int(ano["Start_time"])
+            end_row = int(ano["End_time"])
+            test_dataframes[df_idx]["y"].iloc[start_row:(end_row + 1)] = np.ones(1 + end_row - start_row)
+            
+        #train_df: pd.DataFrame = pd.concat(train_dataframes, axis=0, ignore_index=True)
+        test_df: pd.DataFrame = pd.concat(test_dataframes, axis=0, ignore_index=True)
+        
+        self._data = test_df      
+
 
 def plot_skab():
     
@@ -259,6 +453,7 @@ def plot_msl(channel_name):
     
     return graphJSON    
     
+    
 def plot_smd(channel_name):
     entity = channel_name.replace("smd-", "")
     
@@ -277,3 +472,61 @@ def plot_smd(channel_name):
     
     return graphJSON    
     
+    
+def plot_swat():
+    seed = 0
+    swat = Swat(seed=seed, remove_unique=False, shorten_long=False)
+    test_df = swat.data()
+  
+    title = 'Original Time Series Plot of SWaT'    
+    fig = px.line(test_df, x = test_df.index, y = list(test_df.columns), width=1000, height=500, title=title)
+    #fig_json = fig.to_json()
+    #fig.show()
+
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    print(fig.data[0])
+    
+    return graphJSON    
+    
+    
+def plot_wadi():
+    seed = 0
+    wadi = Wadi(seed=seed)
+    test_df = wadi.data()
+  
+    title = 'Original Time Series Plot of WADI'    
+    fig = px.line(test_df, x = test_df.Date, y = list(test_df.columns), width=1000, height=500, title=title)
+    #fig_json = fig.to_json()
+    #fig.show()
+
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    print(fig.data[0])
+    
+    return graphJSON    
+    
+def plot_damadics():
+    seed = 0
+    damadics = Damadics(seed=seed, drop_init_test=True)
+    test_df = damadics.data()
+  
+    title = 'Original Time Series Plot of damadics'    
+    fig = px.line(test_df, x = test_df.index, y = list(test_df.columns), width=1000, height=500, title=title)
+    #fig_json = fig.to_json()
+    #fig.show()
+
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    print(fig.data[0])
+    
+    return graphJSON        
+
+#main for debugging purposes.     
+#if __name__ == "__main__":
+    #plot_swat()
+    #plot_wadi()
+    #plot_damadics()
+    
+    
+ 
